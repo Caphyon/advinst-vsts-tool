6f0da20b7ecd5366d1054a7e2f50b42993454d4b
Ciprian Burca <ciprian.burca@outlook.com>
2017-08-24 16:32:39 +0300

    Install Advinst on hosted agent.

--- AdvinstTool/AdvinstTool.ts
+++ AdvinstTool/AdvinstTool.ts
@@ -1,76 +1,110 @@
 import * as toolLib from 'vsts-task-tool-lib/tool';
 import * as taskLib from 'vsts-task-lib/task';
-import * as restm from 'typed-rest-client/RestClient';
+import * as taskCmd from 'vsts-task-lib/taskcommand';
 
-import * as os from 'os';
 import * as path from 'path';
+import * as semvish from 'semvish';
+
+let advinstToolName: string = 'advinst';
+let advinstToolArch: string = 'x86';
 
 async function run() {
   try {
 
-    if (osPlat != 'win32')
-      throw new Error('Only Windows system are supported.');
+    if (taskLib.osType() != 'Windows_NT')
+      throw new Error('Only Windows systems are supported.');
+    // Retrieve user inputs
+    let version: string = taskLib.getInput('advinstVersion', true);
+    let license: string = taskLib.getInput('advinstLicense', false);
 
-
-    await getAdvinst('14.x', false);
+    await getAdvinst(version, license);
   }
   catch (error) {
     taskLib.setResult(taskLib.TaskResult.Failed, error.message);
   }
 }
 
+async function getAdvinst(version: string, license: string): Promise<void> {
 
-let osPlat: string = os.platform();
-let osArch: string = os.arch();
+  if ( !semvish.valid(version))
+    throw Error('Invalid version was specified. Version: ' + version);
 
-async function getAdvinst(versionSpec: string, checkLatest: boolean) {
-  console.log('');
-  console.log('--------------------------');
-  console.log('AdvancedInstaller: ' + versionSpec);
-  console.log('--------------------------');
+  let toolPath: string;
+  //Verify if this version of advinst was already installed.
+  toolPath = _getLocalTool(semvish.clean(version));
 
-
-  let toolPath = await acquireAdvinst('14.2');
-  toolLib.prependPath(toolPath);
+  if (!toolPath) {
+    console.log('Aquire a new version: ' + version);
+    //Extract avinst.msi and cache the content.
+    let cachedToolPath: string = await acquireAdvinst(version);
+    //Compute the actual AdvancedInstaller.com folder
+    toolPath = path.join(cachedToolPath, 'bin', 'x86')
+    //Register advinst if a licens key was provided
+    if ( license )
+      await registerAdvinst(toolPath, license);
+    //Add the advinst folder to PATH
+    toolLib.prependPath(toolPath);
+  }
+  else {
+    console.log('Using cached tool. Version: ' + version);
+  }
 }
 
-async function queryLatestMatch(versionSpec: string): Promise<string> {
-  let versions: string[] = [];
-  let version: string = toolLib.evaluateVersions(versions, versionSpec);
-  return version;
+async function registerAdvinst(toolPath: string, license: string): Promise<boolean> {
+  return true;
 }
 
 async function acquireAdvinst(version: string): Promise<string> {
-  //
-  // Download - a tool installer intimately knows how to get the tool (and construct urls)
-  //
-  version = toolLib.cleanVersion(version);
-  let downloadUrl: string = 'http://www.advancedinstaller.com/downloads/advinst.msi';
-  
-  let msiDownloadPath: string = await toolLib.downloadTool(downloadUrl, 'advinst.msi');
-  if (!taskLib.exist(msiDownloadPath)) {
-    throw new Error('Failed to download Advanced Installer tool.');
-  }
-  //
-  // Admin install to extract resources
-  //
-  let advinstWorkFolder = path.join(_getAgentTemp(), 'AdvancedInstaller');
-  taskLib.mkdirP(advinstWorkFolder);
 
-  let msiLogPath: string =  path.join(advinstWorkFolder, 'advinst_install.log');
-  let msiExtractionPath: string = path.join(advinstWorkFolder, 'resources');
-  let msiArguments: string[] = ['/a', msiDownloadPath, 'TARGETDIR=' + msiExtractionPath, '/qn', '/l*v', msiLogPath];
-
-  let exitCode = await taskLib.execSync('msiexec.exe', msiArguments).code;
-  if (exitCode != 0) {
-    throw new Error('Failed to execute msiexec.exe and extract AdvancedInstaller tool. Error: ' + exitCode);
+  let advinstDownloadPath: string = await _downloadAdvinst(version);
+  if (!taskLib.exist(advinstDownloadPath)) {
+    throw new Error('Failed to download tool.');
   }
-  let advinstToolRoot = path.join(msiExtractionPath, 'bin', 'x86');
+
+  let advinstToolRoot = await _extractAdvinst(advinstDownloadPath);
   if (!taskLib.exist(advinstToolRoot)) {
-    throw new Error('Failed to extract Advanced Installer tool.');
+    throw new Error('Failed to extract tool.');
   }
+  console.log('Caching this installed tool.');
+  let cachedToolPath: string = await toolLib.cacheDir(advinstToolRoot, advinstToolName, 
+    semvish.clean(version), advinstToolArch);
+  console.log('Successfully installed tool version ' + version);
 
-  return await toolLib.cacheDir(advinstToolRoot, 'AdvancedInstaller', version);
+  return cachedToolPath;
+}
+
+//
+// Helper methods
+//
+
+function _getLocalTool(version: string) {
+  console.log('Checking if a cached copy exists for this version...');
+  return toolLib.findLocalTool(advinstToolName, version, advinstToolArch);
+}
+
+async function _downloadAdvinst(version: string): Promise<string> {
+  let advinstDownloadUrl = 'http://www.advancedinstaller.com/downloads/' + version + '/advinst.msi';
+  return toolLib.downloadTool(advinstDownloadUrl);
+}
+
+async function _extractAdvinst(sourceMsi: string): Promise<string> {
+
+  let advinstWorkFolder = path.join(_getAgentTemp(), 'AdvancedInstaller');
+  let msiExtractionPath: string = path.join(advinstWorkFolder, 'resources');
+  // Create the work folder, otherwise msiexec will fail because of the log path.
+  if (!taskLib.exist(advinstWorkFolder))
+    taskLib.mkdirP(advinstWorkFolder);
+  let msiLogPath: string = path.join(advinstWorkFolder, 'advinst_install.log');
+
+  let msiexecArguments: string[] = ['/a', sourceMsi, 'TARGETDIR=' + msiExtractionPath, '/qn', '/l*v', msiLogPath];
+
+  let exitCode = await taskLib.exec('msiexec.exe', msiexecArguments);
+  taskLib.command('task.uploadfile', {}, msiLogPath);
+  if (exitCode != 0) {
+    taskLib.command('task.uploadfile', {}, msiLogPath);
+    return '';
+  }
+  return msiExtractionPath;
 }
 
 function _getAgentTemp() {
